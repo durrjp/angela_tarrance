76b66d6d5dd28c876f7d5dc135c3cd664957a7ac
durrjp <durrjp1@gmail.com>
2020-09-24 18:02:39 -0500

    added all houses to firestore, added real pics to carousel

--- src/components/home/CarouselSlider.js
+++ src/components/home/CarouselSlider.js
@@ -1,9 +1,10 @@
 import React from "react"
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import FancyHouse from "../../images/FancyHouse.jpg"
-import Fancyhouse2 from "../../images/FancyHouse2.jpg"
-import Suburbs from "../../images/suburbs.jpg"
+import ValleVerde from "../../images/1641ValleVerde.jpg"
+import Clarence1 from "../../images/4304ClarenceOutside.jpg"
+import Lysander from "../../images/5256LysanderCeiling.jpg"
+import GrannyWhite from "../../images/5444GrannyWhiteBack.jpg"
 import "./Home.css"
 
 export default function CarouselSlider() {
@@ -11,13 +12,16 @@ export default function CarouselSlider() {
         <div>
             <Carousel dynamicHeight={true} interval={6000} transitionTime={800} infiniteLoop autoPlay showThumbs={false}>
                 <div>
-                    <img className="carousel-image" src={FancyHouse} alt="house1" />
+                    <img className="carousel-image" src={ValleVerde} alt="house1" />
                 </div>
                 <div>
-                    <img className="carousel-image" src={Fancyhouse2} alt="house2" />
+                    <img className="carousel-image" src={Clarence1} alt="house2" />
                 </div>
                 <div>
-                    <img className="carousel-image" src={Suburbs} alt="suburbs" />
+                    <img className="carousel-image" src={Lysander} alt="suburbs" />
+                </div>
+                <div>
+                    <img className="carousel-image" src={GrannyWhite} alt="suburbs" />
                 </div>
             </Carousel>
         </div>
