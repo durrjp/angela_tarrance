aaa085310f57d3462f2a07204584557ab2915df7
durrjp <durrjp1@gmail.com>
2020-10-14 17:02:32 -0500

    sending message system complete

--- src/components/contactme/ContactMeForm.js
+++ src/components/contactme/ContactMeForm.js
@@ -1,14 +1,48 @@
-import React from "react"
+import React, { useState } from "react"
 import { Form, FormGroup, Input, Row, Col, Button, Label } from "reactstrap"
 import "./ContactMe.css"
+import * as emailjs from "emailjs-com"
 
 export default function ContactMeForm() {
+    const [fromName, setFromName] = useState("")
+    const [fromEmail, setFromEmail] = useState("")
+    const [message, setMessage] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let template_params = {
+            from_name: `${fromName} - ${fromEmail}`,
+            to_name: "default_service",
+            message_html: message
+
+        }
+
+        emailjs.send(
+            'default_service',
+            'template_18falyi',
+            template_params,
+            'user_tJZEjGIkVYce60J3odJJ5'
+        )
+    }
+
+    const handleNameChange = (value) => {
+        setFromName(value)
+    }
+
+    const handleEmailChange = (value) => {
+        setFromEmail(value)
+    }
+
+    const handleMessageChange = (value) => {
+        setMessage(value)
+    }
+
     return (
         <>
         <div className="contactform-container">
             <div className="container pt-5">
                 <h2>Send me a message!</h2>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <fieldset>
                         <Row>
                             <Col md={6}>
@@ -17,6 +51,7 @@ export default function ContactMeForm() {
                                     <Input
                                         type="text"
                                         className="contactme-input"
+                                        onChange={(e) => handleNameChange(e.target.value)}
                                     />
                                 </FormGroup>
                             </Col>
@@ -26,6 +61,7 @@ export default function ContactMeForm() {
                                     <Input 
                                         type="text"
                                         className="contactme-input"
+                                        onChange={(e) => handleEmailChange(e.target.value)}
                                     />
                                 </FormGroup>
                             </Col>
@@ -37,12 +73,13 @@ export default function ContactMeForm() {
                                     <Input 
                                         type="textarea"
                                         className="contactme-input textarea-input"
+                                        onChange={(e) => handleMessageChange(e.target.value)}
                                     />
                                 </FormGroup>
                             </Col>
                         </Row>
                     </fieldset>
-                    <Button className="sendmessage-button">Send</Button>
+                    <Button className="sendmessage-button" type="submit">Send</Button>
                 </Form>
             </div>
         </div>
